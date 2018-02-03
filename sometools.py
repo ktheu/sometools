d44cd4781e9aedcce5bab21351cd38408c2e0070
jeppard <jonas.eppard@gmail.com>
2018-02-03 17:47:16 +0100

    Added QR-Helper
    Ich weiß nicht ob sie das Aufnehmen sollten "löst" halt das QR-Code Quiz nahezu komplett selbständig

--- sometools.py
+++ sometools.py
@@ -3,7 +3,7 @@ Eine neue Auswahl in die Liste eingaben aufnehmen.
 Eine Abkürzung in Klammern dahinter schreiben. -
 '''
 
-eingaben = ['collatz(col)','ggt','binhextest(bht)']
+eingaben = ['collatz(col)','ggt','binhextest(bht)', 'QR_Helper(QR-H)']
 
 def help_():
     print('Verfügbare Kommandos (Abkürzung in Klammern):')
