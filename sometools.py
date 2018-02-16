2984fec0bb6004151b1a0656426a7f8c16ad15cd
ktheu <khtheuer@t-online.de>
2018-02-16 09:35:12 +0100

    eingaben mit append

--- sometools.py
+++ sometools.py
@@ -3,7 +3,8 @@ Eine neue Auswahl in die Liste eingaben aufnehmen.
 Eine Abkürzung in Klammern dahinter schreiben. -
 '''
 
-eingaben = ['collatz(col)','ggt','binhextest(bht)', 'QR_Helper(QR-H)', 'zeige_unicode_zeichen(zuz)']
+eingaben = ['collatz(col)','ggt','binhextest(bht)', 'QR_Helper(QR-H)']
+eingaben.append('zeige_unicode_zeichen(zuz)')
 
 def help_():
     print('Verfügbare Kommandos (Abkürzung in Klammern):')
